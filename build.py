98612cc8c2278a0bbbf73285300b14a6f14be53a
coded9 <coded9@users.noreply.github.com>
2017-06-11 06:32:30 +0000

    Done

--- build.py
+++ build.py
@@ -7,4 +7,10 @@ def solution(list_of_list):
     '''
     Enter your code here
     '''
+    new_merged_list = []
+    for list1 in list_of_list:
+        for i in list1:
+            new_merged_list.append(i)
     return new_merged_list
+
+#print(solution(original_list))
