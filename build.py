dde6238d9ce0ecb834881cc6bbcb78df9d5924ff
adinathauti <adinathauti@gmail.com>
2017-06-19 18:02:02 +0000

    Done

--- build.py
+++ build.py
@@ -1,10 +1,14 @@
 import itertools
 
-original_list = [[2, 4, 3], [1, 5, 6], [9], [7, 9, 0]]
+ol = [[2, 4, 3], [1, 5, 6], [9], [7, 9, 0]]
 
 
-def solution(list_of_list):
-    '''
-    Enter your code here
-    '''
-    return new_merged_list
+def solution(l):
+    nml = []
+    for i in range(0,len(ol)):
+        for j in range(0,len(ol[i])):
+            nml.append(ol[i][j])
+    print nml
+    return nml
+
+solution(ol)
