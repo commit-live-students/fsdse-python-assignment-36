600f0ad045b656a8302ad80560cb3ea29acc3a66
saahil1292 <saahil1292@gmail.com>
2017-06-23 10:13:35 +0000

    Done

--- build.py
+++ build.py
@@ -1,10 +1,12 @@
-import itertools
-
 original_list = [[2, 4, 3], [1, 5, 6], [9], [7, 9, 0]]
 
-
 def solution(list_of_list):
-    '''
-    Enter your code here
-    '''
+    new_merged_list = []
+    # list_num = [[1,2,3],[4,5],[6],[7],[8,9]]
+    for elem in list_of_list:
+        for num in elem:
+            new_merged_list.append(num)
+    # print concat_num
     return new_merged_list
+
+solution(original_list)
