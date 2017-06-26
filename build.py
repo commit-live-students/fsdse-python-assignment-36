ed4f6154d65f522e10e8e51f71bff570c093720b
datanomnom <ronzzz92@gmail.com>
2017-06-26 12:09:12 +0000

    Done

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ original_list = [[2, 4, 3], [1, 5, 6], [9], [7, 9, 0]]
 
 
 def solution(list_of_list):
-    '''
-    Enter your code here
-    '''
+    new_merged_list = []
+    for i in list_of_list:
+        new_merged_list += i
     return new_merged_list
